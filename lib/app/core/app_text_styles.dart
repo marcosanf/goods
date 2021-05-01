9e464a1c4de7445226bf7333132172f9af78373c
Marcos Fernandes <marcoanf.c@gmail.com>
2021-05-01 19:10:54 -0400

    Customizing text style

--- lib/app/core/app_text_styles.dart
+++ lib/app/core/app_text_styles.dart
@@ -6,7 +6,18 @@ import 'app_colors.dart';
 class AppTextStyles {
   static final TextStyle heading = GoogleFonts.montserrat(
     color: AppColors.white,
-    fontSize: 24.0,
+    fontSize: 32.0,
     fontWeight: FontWeight.w600,
+  );
+  static final TextStyle heading16 = GoogleFonts.montserrat(
+    color: AppColors.white,
+    fontSize: 16.0,
+    fontWeight: FontWeight.w300,
+  );
+
+  static final TextStyle titlePurple = GoogleFonts.montserrat(
+    color: AppColors.lightPurple,
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
   );
 }
