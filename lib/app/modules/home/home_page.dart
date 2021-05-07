e4eacaf9837a86c8db712c600d6e9bf4b055c37d
Marcos Fernandes <marcoanf.c@gmail.com>
2021-05-07 01:01:37 -0400

    product card customization

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -4,6 +4,7 @@ import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:goodz/app/core/app_colors.dart';
 import 'package:goodz/app/core/app_text_styles.dart';
+import 'package:goodz/app/model/product.dart';
 import 'package:goodz/app/modules/home/widgets/app_bar_widget.dart';
 import 'package:goodz/app/modules/home/widgets/bottom_nav_bar_widget.dart';
 import 'package:goodz/app/modules/home/widgets/product_form_widget.dart';
@@ -17,106 +18,101 @@ class HomePage extends GetView<HomeController> {
         if (notification.scrollDelta!.sign == 1) {
           controller.yTransValue = 100;
         } else if (notification.scrollDelta!.sign == -1) {
-          controller.yTransValue = 100;
+          controller.yTransValue = 0;
         }
         return true;
       },
       child: Scaffold(
+        backgroundColor: Color(0xFFf0f2f5),
         resizeToAvoidBottomInset: false,
         appBar: AppBarWidget(),
         body: controller.products.isNotEmpty
-            ? GetBuilder(
-                builder: (HomeController homeController) {
-                  return Container(
-                    child: Column(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: Text('Meus produtos'),
+            ? GetBuilder(builder: (HomeController controller) {
+                return ListView.builder(
+                  itemCount: controller.products.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: 120.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25.0),
+                          color: AppColors.white,
                         ),
-                        Expanded(
-                          flex: 12,
-                          child: GridView.builder(
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 2),
-                            itemCount: controller.products.length,
-                            itemBuilder: (context, index) {
-                              return Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Container(
-                                  height: 210,
-                                  width: 150,
-                                  decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.circular(25),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: AppColors.border,
-                                          offset: Offset(1, 2),
-                                        ),
-                                      ]),
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            Text(
-                                              homeController
-                                                  .products[index].amount
-                                                  .toString(),
-                                            ),
-                                            Text(
-                                              homeController
-                                                  .products[index].price
-                                                  .toString(),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                      Icon(FontAwesomeIcons.boxes),
-                                      Column(
-                                        children: [
-                                          Text(homeController
-                                              .products[index].name!),
-                                          Text(homeController
-                                              .products[index].code!),
-                                        ],
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            Icon(FontAwesomeIcons.heart),
-                                            GestureDetector(
-                                              onTap: () => controller
-                                                  .deleteProduct(controller
-                                                      .products[index].id!),
-                                              child: Icon(FontAwesomeIcons
-                                                  .solidTrashAlt),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ),
+                        child: Row(
+                          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              height: 120.0,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(25.0),
+                                  topLeft: Radius.circular(25.0),
                                 ),
-                              );
-                            },
-                          ),
+                              ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(25.0),
+                                child:
+                                    Image.asset('assets/images/product.webp'),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 8.0,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  controller.products[index].name!,
+                                  style: AppTextStyles.bodyBold,
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      'Código: ${controller.products[index].code!}',
+                                      style: AppTextStyles.body12,
+                                    ),
+                                    Text(
+                                      ' | ',
+                                      style: AppTextStyles.body12,
+                                    ),
+                                    Text(
+                                      'Quantidade: ${controller.products[index].amount.toString()}',
+                                      style: AppTextStyles.body12,
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      'R\$ ${controller.products[index].price.toString()}',
+                                      style: AppTextStyles.bodyGrey20,
+                                    ),
+                                    SizedBox(
+                                      width: 100.0,
+                                    ),
+                                    GestureDetector(
+                                      onTap: () {
+                                        controller.deleteProduct(
+                                            controller.products[index].id!);
+                                      },
+                                      child: Text(
+                                        'Remover',
+                                        style: AppTextStyles.bodyDarkRed,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  );
-                },
-              )
+                      ),
+                    );
+                  },
+                );
+              })
             : Container(
                 child: Center(
                   child: Column(
